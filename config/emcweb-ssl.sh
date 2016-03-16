8eadf8e53da5925fda6d1d7454a50441ffaead6d
Sergii Vakula <sv@aspanta.com>
2016-03-16 19:30:56 +0200

    Remove emcweb-ssh.sh

--- config/emcweb-ssl.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-grep -v ^exit /etc/rc.local > /etc/rc.local.new
-echo -e "[ ! -f /etc/ssl/private/emcweb.key ] || [ ! -f /etc/ssl/certs/emcweb.crt ] && openssl req -nodes -x509 -newkey rsa:4096 -keyout /etc/ssl/private/emcweb.key -out /etc/ssl/certs/emcweb.crt -days 3560 -subj /C=US/ST=Oregon/L=Portland/O=IT/CN=emercoin.local\n\nexit 0\n" >> /etc/rc.local.new
-mv -f /etc/rc.local.new /etc/rc.local
-chmod +x /etc/rc.local
